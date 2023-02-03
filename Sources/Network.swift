4c17d520fb87d59afbc7d254f904febc064c1687
Evan Cooper <evan.cooper@rogers.com>
2023-02-02 21:02:23 -0500

    Add async/await

--- Sources/Network.swift
+++ Sources/Network.swift
@@ -1,3 +1,4 @@
+import Combine
 import Foundation
 
 public protocol Network {
@@ -15,5 +16,33 @@ public extension Network {
     @discardableResult
     func send<T: Request>(_ request: T, withPriority priority: Priority = .normal, completion: ((Result<T.Response, Error>) -> Void)? = nil) -> Cancellable {
         send(request, withPriority: priority, completion: completion)
+    }
+}
+
+public extension Network {
+    func send<T: Request>(_ request: T, withPriority priority: Priority = .normal) -> AnyPublisher<T.Response, Error> {
+        let subject = PassthroughSubject<T.Response, Error>()
+        let cancellable = self.send(request, withPriority: priority) { result in
+            switch result {
+            case .failure(let error):
+                subject.send(completion: .failure(error))
+            case .success(let response):
+                subject.send(response)
+                subject.send(completion: .finished)
+            }
+        }
+        return subject
+            .handleEvents(receiveCancel: { [cancellable] in cancellable.cancel() })
+            .eraseToAnyPublisher()
+    }
+}
+
+public extension Network {
+    func send<T: Request>(_ request: T, withPriority priority: Priority = .normal) async throws -> T.Response {
+        try await withCheckedThrowingContinuation { continuation in
+            send(request, withPriority: priority) { result in
+                continuation.resume(with: result)
+            }
+        }
     }
 }
